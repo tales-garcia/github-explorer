3716bf19b3b84d9e9f67c6011de789a74dc7d578
Tales Garcia <garcia.tales@gmail.com>
2020-11-18 21:25:53 -0300

    feat(global style): add global style to application

---
+++ src/styles/global.ts
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from "styled-components";
+import githubBackground from '../images/github.svg';
+
+export default createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        outline: 0;
+    }
+    body {
+        background: #F0F0F5 url(${githubBackground}) no-repeat right top;
+        -webkit-font-smoothing: antialiased;
+    }
+    body, input, button {
+        font-size: 16px;
+        font-family: Roboto, sans-serif;
+    }
+    #root {
+        max-width: 960px;
+    }
+`;
