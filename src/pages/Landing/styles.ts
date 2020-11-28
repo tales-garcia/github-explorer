e06842ca6c0d65b2cfb2e09b1ff0f518b3f282ce
Tales Garcia <garcia.tales@gmail.com>
2020-11-28 09:58:41 -0300

    fix(styles): fix images strecthing bug
    Profile images are stretching, adding flex: 1 will resolve this bug. Switch from left margin to
    horizontal margin for better text collapsing

--- src/pages/Landing/styles.ts
+++ src/pages/Landing/styles.ts
@@ -71,7 +71,8 @@ export const Repositories = styled.div`
             border-radius: 50%;
         }
         div {
-            margin-left: 16px;
+            margin: 0 16px;
+            flex: 1;
 
             strong {
                 font-size: 20px;
